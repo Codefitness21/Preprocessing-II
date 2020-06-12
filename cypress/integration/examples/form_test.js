ff286837b1d77a846930df4ecc35dd699f2fc0c6
Kelly <kelly.m.hill2115@gmail.com>
2020-06-11 20:25:18 -0500

    Commit

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -5,4 +5,10 @@ it ('visits the Fun Bus app', function() {
     cy.visit("https://codefitness21.github.io/Preprocessing-II/")
 });
 
-})
+it("second test", function () {
+    cy.get(`[data-cy="name"]`).type("Fun Bus").should("have.value", "Emi")	
+});
+
+it("Second it Test", function () {});
+
+})	
